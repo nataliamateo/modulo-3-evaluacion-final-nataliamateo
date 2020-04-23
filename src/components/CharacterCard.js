8572a10c859093a1c340f6f827964295c3808a9a
Natalia Mateo <nmateo7@gmail.com>
2020-04-23 11:47:21 +0200

    Create Character card and list styles

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import '../stylesheets/CharacterCard.scss';
 
 const CharacterCard = (props) => {
   return (
-    <div className='CharacterCard'>
-      <img src={props.character.image} alt={`Foto de ${props.character.name}`} title={`Foto de ${props.character.name}`} />
-      <p>{props.character.name}</p>
-      <p>{props.character.species}</p>
-    </div>
+    <li className='card'>
+      <img className='card__image' src={props.character.image} alt={`Foto de ${props.character.name}`} title={`Foto de ${props.character.name}`} />
+      <p className='card__name'>{props.character.name}</p>
+      <p className='card__species'>{props.character.species}</p>
+    </li>
   );
 };
 
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import CharacterCard from './CharacterCard.js';
+import '../stylesheets/CharacterList.scss';
 
 const CharacterList = (props) => {
-  const characterElements = props.characters.map((character) => (
-    <li key={character.id}>
-      <CharacterCard character={character} />
-    </li>
-  ));
-  return <ul>{characterElements}</ul>;
+  const characterElements = props.characters.map((character) => {
+    return <CharacterCard key={character.id} character={character} />;
+  });
+  return (
+    <section>
+      <ul className='cards'>{characterElements}</ul>
+    </section>
+  );
 };
 
 export default CharacterList;
