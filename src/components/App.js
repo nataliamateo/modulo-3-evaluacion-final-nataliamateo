8a9615ac88450fd739d15783b71ef59ad6ec360e
Natalia Mateo <nmateo7@gmail.com>
2020-04-23 09:51:48 +0200

    Create header and styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import '../stylesheets/App.scss';
 import Header from './Header.js';
-import Filters from './Filters.js';
 import CharacterList from './CharacterList.js';
 import getDatafromApi from '../services/api.js';
 
@@ -16,7 +15,6 @@ const App = () => {
     <>
       <Header />
       <main>
-        <Filters />
         <CharacterList characters={characters} />
       </main>
     </>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import '../stylesheets/Header.scss';
+import Filters from './Filters';
 
 const Header = () => {
-  return <header className='header'></header>;
+  return (
+    <header className='header'>
+      <div className='header__background'></div>
+      <Filters className='header__form' />
+    </header>
+  );
 };
 
 export default Header;
