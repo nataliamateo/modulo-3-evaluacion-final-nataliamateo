8cebfddd8ff5076f08b79d50b434dcb447e6cee9
Natalia Mateo <nmateo7@gmail.com>
2020-04-23 13:35:41 +0200

    Update docs

---
+++ docs/precache-manifest.163676cfda9d23e332ccd48e55d157c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95c9c3d9573ad236b7127e7809b67658",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3ee48e2a35abf57db4d1",
+    "url": "./static/css/main.25173d2e.chunk.css"
+  },
+  {
+    "revision": "8ca2d3fbe6f7d06d660c",
+    "url": "./static/js/2.fef0c97a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.fef0c97a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ee48e2a35abf57db4d1",
+    "url": "./static/js/main.86c34534.chunk.js"
+  },
+  {
+    "revision": "cbb7502546e28b5dbd02",
+    "url": "./static/js/runtime-main.5c2b7656.js"
+  },
+  {
+    "revision": "3d4a07a4ec039e359a816c3d397b8cfa",
+    "url": "./static/media/rick-and-morty.3d4a07a4.png"
+  }
+]);
