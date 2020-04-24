f766c4e3b2911b9dccca17a1700baf6a15613b76
Natalia Mateo <nmateo7@gmail.com>
2020-04-24 12:25:07 +0200

    Add icons in a character detail with conditionals

--- src/components/CharacterDetailPj.js
+++ src/components/CharacterDetailPj.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import '../stylesheets/CharacterDetailPj.scss';
 import { Link } from 'react-router-dom';
+import Alien from '../images/alien.png';
 
 const CharacterDetailPj = (props) => {
+  const getStatus = () => {
+    if (props.character.status === 'Alive') {
+      return <i className='fas fa-heartbeat'></i>;
+    } else if (props.character.status === 'Dead') {
+      return <i className='far fa-dizzy'></i>;
+    } else {
+      return <i className='fas fa-question'></i>;
+    }
+  };
+
+  const getSpecies = () => {
+    return props.character.species === 'Human' ? <i className='fas fa-male'></i> : <img className='alien' src={Alien} alt='Icono de alien' />;
+  };
+
   return (
     <div className='detail'>
       <div className='detail__card'>
@@ -16,8 +31,13 @@ const CharacterDetailPj = (props) => {
           <div className='container'>
             <ul className='container__list'>
               <li className='container__list--item'> {props.character.name}</li>
-              <li className='container__list--item'>Status: {props.character.status}</li>
-              <li className='container__list--item'>Especie: {props.character.species}</li>
+              <li className='container__list--item'>
+                Status: {props.character.status} {getStatus()}
+              </li>
+              <li className='container__list--item'>
+                Especie: {props.character.species}
+                {getSpecies()}
+              </li>
               <li className='container__list--item'>Origen: {props.character.origin}</li>
               <li className='container__list--item'>Espisodios: {props.character.episode}</li>
             </ul>
