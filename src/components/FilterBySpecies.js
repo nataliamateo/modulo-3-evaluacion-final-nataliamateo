fae1347cb65364768bf657f24aeea55c7c3a513a
Natalia Mateo <nmateo7@gmail.com>
2020-04-24 13:27:58 +0200

    Create filter by species styles

--- src/components/FilterBySpecies.js
+++ src/components/FilterBySpecies.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../stylesheets/FilterByName.scss';
+import '../stylesheets/FilterBySpecies.scss';
 
 const FilterByName = (props) => {
   const handleChange = (ev) => {
@@ -12,18 +12,28 @@ const FilterByName = (props) => {
 
   return (
     <>
-      <h2>Especie</h2>
-      <div>
-        <label htmlFor='all'>Todos</label>
-        <input className='' id='Species' name='Species' type='radio' value='all' onChange={handleChange} defaultChecked></input>
-      </div>
-      <div>
-        <label htmlFor='Human'>Humano</label>
-        <input className='' id='Species' name='Species' type='radio' value='Human' onChange={handleChange}></input>
-      </div>
-      <div>
-        <label htmlFor='Alien'>Alien</label>
-        <input className='' id='Species' name='Species' type='radio' value='Alien' onChange={handleChange}></input>
+      <div className='species'>
+        <h2 className='species__title'>Especies:</h2>
+        <div className='species__radios'>
+          <div>
+            <label className='species__radios--title' htmlFor='all'>
+              Todos
+            </label>
+            <input className='species__radios--all' id='Species' name='Species' type='radio' value='all' onChange={handleChange} defaultChecked></input>
+          </div>
+          <div>
+            <label className='species__radios--title' htmlFor='Human'>
+              Humanos
+            </label>
+            <input className='species__radios--all' id='Species' name='Species' type='radio' value='Human' onChange={handleChange}></input>
+          </div>
+          <div>
+            <label className='species__radios--title' htmlFor='Alien'>
+              Aliens
+            </label>
+            <input className='species__radios--all' id='Species' name='Species' type='radio' value='Alien' onChange={handleChange}></input>
+          </div>
+        </div>
       </div>
     </>
   );
