174ae72483909b516190fb88191ff60e22c85c23
Natalia Mateo <nmateo7@gmail.com>
2020-05-04 11:09:09 +0200

    Update docs

--- docs/precache-manifest.56ffaea5638b1ce0beda602df2b27f0c.js
+++ docs/precache-manifest.56ffaea5638b1ce0beda602df2b27f0c.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "da3d8a9607ac325225f22874d4a411da",
+    "revision": "8486a7c6b45625790ea21d849feaedda",
     "url": "./index.html"
   },
   {
-    "revision": "de8ef55b2d97c4578376",
-    "url": "./static/css/main.aea2bd2c.chunk.css"
+    "revision": "98fac654efc3b9ca9303",
+    "url": "./static/css/main.8c8e16f6.chunk.css"
   },
   {
-    "revision": "3e2ede2a9f99d4075288",
-    "url": "./static/js/2.7d0c3ae1.chunk.js"
+    "revision": "9e8e2c45d0010e607d7f",
+    "url": "./static/js/2.b8cd0b80.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "./static/js/2.7d0c3ae1.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.b8cd0b80.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "de8ef55b2d97c4578376",
-    "url": "./static/js/main.0d177620.chunk.js"
+    "revision": "98fac654efc3b9ca9303",
+    "url": "./static/js/main.5356dcbb.chunk.js"
   },
   {
     "revision": "cbb7502546e28b5dbd02",
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.b179b15336c134a7523486e2998213d8.js"
+  "./precache-manifest.56ffaea5638b1ce0beda602df2b27f0c.js"
 );
 
 self.addEventListener('message', (event) => {
